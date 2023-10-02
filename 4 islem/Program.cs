2310d09fe4919b450af47c0a0e63cf0352c43d39
Panaria <baranyldzby@gmail.com>
2023-10-02 21:13:19 +0300

    Add project files.

---
+++ 4 islem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_islem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Toplama - Çıkarma - Çarpma - Bölme
+
+            Console.WriteLine(6 + 1);
+            Console.WriteLine(8 - 1);
+            Console.WriteLine(7 * 1);
+            Console.WriteLine(49 / 7);
+
+            Console.ReadLine();
+        }
+    }
+}
